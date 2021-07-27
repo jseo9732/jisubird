ceb3ff7eccdf1777d09e14b813eab48ce216446f
jseo9732 <jseo97320@gmail.com>
2021-07-27 22:03:58 +0900

    First Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config(); //.env 연결(?)
+const pageRouter = require('./routes/page');
+const { sequelize } = require('./models');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+//넉적스 설정 방법
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+//모델과 서버 연결하기
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+app.use(passport.initialize()); //요청 객체에 passport 설정을 심음
+app.use(passport.session()); // req.sesseion 객체에 passport 정보를 저장
+
+app.use('/', pageRouter);
+
+//404 처리 미들웨어
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+//에러처리 미들웨어
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//포트 배정
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
---
+++ passport/index.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const local = require('./localStrategy')
+const kakao = require('./kakaoStrategy')
+const User = require('../models/user')
+
+module.exports = () => {
+    //사용자 정보 객체를 세션에 아이디로 저장
+    passport.serializeUser((user, done) => {
+        done(null, user.id); //메모리 저장 용량을 줄이기 위해 id만 저장
+    });
+
+    //세션에 저장한 아이디를 통해서 사용자 정보 객체를 불러옴
+    passport.deserializeUser((id, done) => {
+        User.findOne({ where: { id } })
+            .then(user => done(nul, user))
+            .catch(err => done(err));
+    });
+
+    local();
+    kakao();
+};
