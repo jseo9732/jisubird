a32b1f5f576915283b8c65818825a6d21eccc13c
jseo9732 <jseo9732@gmail.com>
2021-07-28 18:13:43 +0900

    오타 수정

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,16 +1,16 @@
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) { //로그인 되어있는지 확인 true or false로 반환
-        next();
+      next();
     } else {
-        res.statuss(403).send('로그인 필요');
+      res.status(403).send('로그인 필요');
     }
-};
-
-exports.isNotLoggedIn = (req, res, next) => {
+  };
+  
+  exports.isNotLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) { //로그인 되어있는지 확인 true or false로 반환
-        next();
+      next();
     } else {
-        const message = encodeURIComponent('로그인한 상태입니다.') //encodeURIComponent: 모든 문자를 인코딩하는 함수입니다.
-        res.redirect(`/?error=${message}`);
+      const message = encodeURIComponent('로그인한 상태입니다.'); //encodeURIComponent: 모든 문자를 인코딩하는 함수입니다.
+      res.redirect(`/?error=${message}`);
     }
-};
+  };
