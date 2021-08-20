c1ab8056221116368b35b5d0403d2b6d5d8b8f8e
dkstjgus <83899691+dkstjgus@users.noreply.github.com>
2021-08-21 02:18:45 +0900

    좋아요 추가

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const User = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
 const Comment = require('./comment');
+const Like = require('./like');
 
 const db = {};
 const sequelize = new Sequelize(
@@ -16,15 +17,18 @@ db.User = User;
 db.Post = Post;
 db.Hashtag = Hashtag;
 db.Comment = Comment;
+db.Like = Like;
 
 User.init(sequelize);
 Post.init(sequelize);
 Hashtag.init(sequelize);
 Comment.init(sequelize);
+Like.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
 Hashtag.associate(db);
 Comment.associate(db);
+Like.associate(db);
 
 module.exports = db;
