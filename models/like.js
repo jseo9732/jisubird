c604cdf38246eed77f9680746dc883fa05d0766f
dkstjgus <83899691+dkstjgus@users.noreply.github.com>
2021-08-21 02:17:15 +0900

    좋아요 DB //오류

---
+++ models/like.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Post extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      allowNull: true,
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'Like',
+      tableName: 'Likes',
+      paranoid: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+    });
+  }
+
+  static associate(db) {
+    db.User.belongsToMany(db.Post,{
+        foreignKey : 'likerId',
+        as: 'LikedPosts',
+        through : 'Like',
+    });
+    db.Post.belongsToMany(db.User,{
+        foreignKey: 'likedPostId',
+        as : 'Likers',
+        through : 'Like',
+    });
+  }
+};
